a1c9645cc7e6b10185bbc3fa1f61991eacdec72a
Lexa <up201604741@fe.up.pt>
2021-08-28 15:54:24 +0100

    feat: generate action-component

---
+++ component-library-template-develop/src/components/action-component/action-component.tsx
@@ -0,0 +1,17 @@
+import { Component, Host, h } from '@stencil/core';
+
+@Component({
+  tag: 'action-component',
+  styleUrl: 'action-component.scss',
+  shadow: true,
+})
+export class ActionComponent {
+  // eslint-disable-next-line class-methods-use-this
+  render() {
+    return (
+      <Host>
+
+      </Host>
+    );
+  }
+}
