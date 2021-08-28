329180cf5bc40345222144a8d448a60485d5bd65
Lexa <up201604741@fe.up.pt>
2021-08-28 16:26:57 +0100

    feat: add whole component HTML layout

--- component-library-template-develop/src/components/action-component/action-component.tsx
+++ component-library-template-develop/src/components/action-component/action-component.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, State } from '@stencil/core';
 
 @Component({
   tag: 'action-component',
@@ -6,11 +6,43 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class ActionComponent {
+
+  @State() open: boolean=false;
+
   // eslint-disable-next-line class-methods-use-this
   render() {
     return (
       <Host>
-
+        <div class="HeadlineContainer">
+          <div class="HeadlineWrapper">
+            <div class="HeadlineText">
+              <h1> CtA - Main conversion </h1>
+            </div>
+          </div>
+        </div>
+        <div class="MainContentContainer">
+          <div class="InformationContainer">
+            <div class="InformationWrapper"> 
+              <p class="InformationText"> A wonderful serenity has taken possession </p>
+              <p> A wonderful serenity has taken possession of my entire soul, like these sweet moments </p>
+              <button type="button" name="link_button" class="link_btn" onClick={() => this.open=true}> Link button </button>
+            </div>
+          </div>
+          <div class="ImageContainer">
+            {/** ADD LOCAL IMAGE*/}
+          </div>
+        </div>
+        <div hidden={!this.open} class="ContactContainer">
+          <div class="ContactWrapper">
+            <div class="ContactHeader">
+              <div class="ArrowUp"></div>
+              <p class="ContactText"> Contact us </p>
+            </div>
+          </div>
+          <button class="close-button" aria-label="Close alert" type="button"  onClick={() => this.open=false} data-close>
+            <span aria-hidden="true">&times;</span> Close
+          </button>
+        </div>
       </Host>
     );
   }
